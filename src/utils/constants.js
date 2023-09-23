a82a50c10d0fb8ec94544429f9859ac5690d77f0
Harrshaa <sriharsha.mutakodoor@gmail.com>
2023-09-24 01:39:00 +0530

    Logo Change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const LOGO_URL =
-  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+  "https://img.freepik.com/free-vector/delivery-service-illustrated_23-2148505081.jpg?size=626&ext=jpg&ga=GA1.2.1568887026.1695475277&semt=ais";
 
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
